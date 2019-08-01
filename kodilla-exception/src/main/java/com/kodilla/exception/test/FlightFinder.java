bc1f876a7fd78e42bef8d851dd7190b65637f518
grzegorzGregorion <grzegorz.p.skwarna@gmail.com>
2019-08-01 19:26:02 +0200

    Finish 8.4 Kodilla task

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -18,7 +18,7 @@ public class FlightFinder {
         if (airportsMap.containsKey(flight.getArrivalAirport())) {
             return airportsMap.get(flight.getArrivalAirport());
         } else {
-            throw new RouteNotFoundException();
+            throw new RouteNotFoundException("Route not available due to airport being closed!");
         }
     }
 
@@ -29,13 +29,16 @@ public class FlightFinder {
         Flight flight4 = new Flight("New York", "Tokio");
         Flight flight5 = new Flight("New York", "Paris");
         Flight flight6 = new Flight("New York", "Roma");
-        Optional<String> myString = Optional.ofNullable("not null");
-        System.out.println(myString.orElse("example text"));
+
         FlightFinder flightFinder = new FlightFinder();
         try {
+            System.out.println(flightFinder.findTheFlight(flight6));
             System.out.println(flightFinder.findTheFlight(flight3));
+            System.out.println(flightFinder.findTheFlight(flight5));
         } catch (RouteNotFoundException e) {
-            System.out.println("Route not found! Airport...");
+            System.out.println(e.exceptionMessage);
+        } finally {
+            System.out.println("Program finished work correctly!");
         }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -1,4 +1,9 @@
 package com.kodilla.exception.test;
 
 public class RouteNotFoundException extends Exception {
+    String exceptionMessage;
+
+    public RouteNotFoundException(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
 }
