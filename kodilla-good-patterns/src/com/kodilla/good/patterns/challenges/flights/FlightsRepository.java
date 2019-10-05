f2898d89a03f66556fc1bb58ea425399e8cf0cd8
grzegorzGregorion <grzegorz.p.skwarna@gmail.com>
2019-10-05 11:45:41 +0200

    Remove spare comments

--- kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/flights/FlightsRepository.java
+++ kodilla-good-patterns/src/com/kodilla/good/patterns/challenges/flights/FlightsRepository.java
@@ -1,29 +1,3 @@
-//package com.kodilla.good.patterns.challenges.flights;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class FlightsRepository {
-//    private final List<Flight> flightList = new ArrayList<>();
-//
-//    public FlightsRepository() {
-//        flightList.add(new Flight("Oslo", "New York"));
-//        flightList.add(new Flight("New York", "Warsaw"));
-//        flightList.add(new Flight("Paris", "Moscow"));
-//        flightList.add(new Flight("Barcelona", "Zurich"));
-//        flightList.add(new Flight("Krakow", "Tokio"));
-//        flightList.add(new Flight("Paris", "Barcelona"));
-//        flightList.add(new Flight("Madrid", "Paris"));
-//        flightList.add(new Flight("Roma", "Roma"));
-//        flightList.add(new Flight("Gdansk", "Warsaw"));
-//        flightList.add(new Flight("Barcelona", "Washington"));
-//    }
-//
-//    public List<Flight> getFlightList() {
-//        return new ArrayList<>(flightList);
-//    }
-//}
-
 package com.kodilla.good.patterns.challenges.flights;
 
 import java.util.ArrayList;
