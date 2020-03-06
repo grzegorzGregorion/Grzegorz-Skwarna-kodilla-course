3900d8c48abb780b98b9777a79aacedf6637709d
grzegorzGregorion <grzegorz.p.skwarna@gmail.com>
2020-03-06 21:02:51 +0100

    Correct task 24.5 acc to Mentor remarks

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/BookStatisticsAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/BookStatisticsAdapter.java
@@ -1,8 +1,6 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
 import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
-import com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book;
-import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +11,7 @@ public class BookStatisticsAdapter extends BookStatisticsAdaptee implements Clas
     public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclassifier.librarya.Book> bookSet) {
         Map<com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> books = new HashMap<>();
         for (com.kodilla.patterns2.adapter.bookclassifier.librarya.Book set : bookSet) {
-            books.put(new BookSignature(set.getSignature()), new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(set.getAuthor(), set.getTitle(), set.getPublicationYear()));
+            books.put(new com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature(set.getSignature()), new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(set.getAuthor(), set.getTitle(), set.getPublicationYear()));
         }
 return medianPublicationYear(books);
     }
